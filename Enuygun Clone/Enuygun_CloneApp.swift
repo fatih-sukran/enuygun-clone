718662eee23d89aa836508b2f1c0c0bb385e4050
fatih-sukran <fth.21.81@gmail.com>
2023-08-18 20:09:07 +0300

    Initial Commit

---
+++ Enuygun Clone/Enuygun_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enuygun_CloneApp.swift
+//  Enuygun Clone
+//
+//  Created by Fatih Şükran on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Enuygun_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
